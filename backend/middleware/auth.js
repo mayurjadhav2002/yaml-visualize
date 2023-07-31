76840963e800b834be9273d9a64e8d6e75b16dc8
mayurjadhav2002 <mayurshrikantjadhav@gmail.com>
2023-08-01 00:14:27 +0530

    Task #3 | User authentication

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,7 +1,5 @@
 const jwt = require("jsonwebtoken");
 
-const config = require('../config/config')
-
 const verifyToken = (req, res, next) => {
   const token =
     req.body.token || req.query.token || req.headers["Authorization"];
@@ -10,7 +8,7 @@ const verifyToken = (req, res, next) => {
     return res.status(403).send("A token is required for authentication");
   }
   try {
-    const decoded = jwt.verify(token, config.secret_jwt);
+    const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
     req.user = decoded;
   } catch (err) {
     return res.status(401).send("Invalid Token");
